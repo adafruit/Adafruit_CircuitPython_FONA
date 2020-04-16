9b255b70791562552ad886435f6790a5985b1284
brentru <robots199@me.com>
2020-04-16 12:15:38 -0400

    remove module specific tests, start to abstract for socket

--- examples/fona_simpletest.py
+++ examples/fona_simpletest.py
@@ -2,16 +2,12 @@ import time
 import board
 import busio
 import digitalio
-import adafruit_fona
+from adafruit_fona.adafruit_fona import FONA
+import adafruit_fona.adafruit_fona_socket as socket
 
-# rst pin
-rst = digitalio.DigitalInOut(board.D4)
+TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+JSON_URL = "http://api.coindesk.com/v1/bpi/currentprice/USD.json"
 
-# Initialize adafruit fona
-print("Initializing FONA... (this may take 3 seconds)")
-fona = adafruit_fona.FONA(board.TX, board.RX, rst, debug=True)
-
-### Cellular ###
 # Get GPRS details and more from a secrets.py file
 try:
     from secrets import secrets
@@ -19,41 +15,62 @@ except ImportError:
     print("GPRS secrets are kept in secrets.py, please add them there!")
     raise
 
+# Initialize FONA module (this may take a few seconds)
+rst = digitalio.DigitalInOut(board.D4)
+fona = FONA(board.TX, board.RX, rst, debug=True)
 
-## GPS ##
-print("Turning on GPS...")
-fona.GPS
-fona.GPS = True
+print("FONA WebClient Test")
 
-time.sleep(2) # wait for a fix
-fona.GPS
+# Enable GPS
+fona.gps = True
+# wait for a fix
+# TODO: Change this to wait for a fix instead!
+time.sleep(7)
 
-
-## GPRS ##
-fona.set_GRPS((secrets["apn"], secrets["apn_username"], secrets["apn_password"]))
-
-print("GRPS ON? ", fona.GPRS)
-
-# ensure we're connected to a cellular network
+# Bring up cellular connection
+fona.set_gprs((secrets["apn"], secrets["apn_username"], secrets["apn_password"]))
 while fona.network_status != 1:
     print("Not registered to a network, waiting...")
     time.sleep(5)
-
 time.sleep(5)
 
-fona.GPRS = True
+# Bring up GPRS
+fona.gprs = True
+time.sleep(6)
 
-print("GRPS ON? ", fona.GPRS)
+print("Local IP: ", fona.local_ip)
 
-time.sleep(10)
+"""
+socket.set_interface(eth)
 
-## Website - HTTP ##
-data_buf = b""
-url = "wifitest.adafruit.com/testwifi/index.html"
+# Create a new socket
+sock = socket.socket()
 
-# GET URL
-data_buf = fona.http_get(url, data_buf)[1]
-if not len(data_buf):
-    print("Unable to GET data.")
-print(data_buf)
+print("Connecting to: ", SERVER_ADDRESS[0])
+sock.connect(SERVER_ADDRESS)
 
+print("Connected to ", sock.getpeername())
+
+# Make a HTTP Request
+sock.send(b"GET /testwifi/index.html HTTP/1.1\n")
+sock.send(b"Host: 104.236.193.178\n")
+sock.send(b"Connection: close\n\n")
+
+# Start transmission timer
+start = time.monotonic()
+
+bytes_avail = 0
+while not bytes_avail:
+    bytes_avail = sock.available()
+    if bytes_avail > 0:
+        data = sock.recv(bytes_avail)
+        print(data[0])
+        break
+    time.sleep(0.05)
+
+end = time.monotonic()
+print("Received: %d bytes"%bytes_avail)
+end = end - start / 1000000.0
+rate = bytes_avail / end / 1000.0
+print("Rate = %0.5f kbytes/second"%rate)
+"""
