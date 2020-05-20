5f10a2d32196a68bc2ff0dd930dc5fa1443f3253
brentru <robots199@me.com>
2020-05-20 15:55:23 -0400

    make uart functions private, remove public in_waiting method

--- adafruit_fona/adafruit_fona.py
+++ adafruit_fona/adafruit_fona.py
@@ -93,7 +93,8 @@ class FONA:
         self._rst = rst
         self._rst.switch_to_output()
         self._ri = ri
-        self._ri.switch_to_input()
+        if self._ri is not None:
+            self._ri.switch_to_input()
         if not self._init_fona():
             raise RuntimeError("Unable to find FONA. Please check connections.")
 
@@ -135,8 +136,8 @@ class FONA:
         self._buf = b""
         self._uart.reset_input_buffer()
 
-        self.uart_write(b"ATI\r\n")
-        self.read_line(multiline=True)
+        self._uart_write(b"ATI\r\n")
+        self._read_line(multiline=True)
 
         if self._buf.find(b"SIM808 R14") != -1:
             self._fona_type = FONA_808_V2
@@ -149,8 +150,8 @@ class FONA:
 
         if self._fona_type == FONA_800_L:
             # determine if SIM800H
-            self.uart_write(b"AT+GMM\r\n")
-            self.read_line(multiline=True)
+            self._uart_write(b"AT+GMM\r\n")
+            self._read_line(multiline=True)
 
             if self._buf.find(b"SIM800H") != -1:
                 self._fona_type = FONA_800_H
@@ -158,7 +159,7 @@ class FONA:
 
     def factory_reset(self):
         """Resets modem to factory configuration."""
-        self.uart_write(b"ATZ\r\n")
+        self._uart_write(b"ATZ\r\n")
 
         if not self._expect_reply(REPLY_OK):
             return False
@@ -190,16 +191,16 @@ class FONA:
             print("FONA IEMI")
         self._uart.reset_input_buffer()
 
-        self.uart_write(b"AT+GSN\r\n")
-        self.read_line(multiline=True)
+        self._uart_write(b"AT+GSN\r\n")
+        self._read_line(multiline=True)
         iemi = self._buf[0:15]
         return iemi.decode("utf-8")
 
     @property
     def local_ip(self):
         """Returns the local IP Address, False if not set."""
-        self.uart_write(b"AT+CIFSR\r\n")
-        self.read_line()
+        self._uart_write(b"AT+CIFSR\r\n")
+        self._read_line()
         try:
             ip_addr = self.pretty_ip(self._buf)
         except ValueError:
@@ -211,10 +212,10 @@ class FONA:
         """Returns SIM Card's ICCID number as a string."""
         if self._debug:
             print("ICCID")
-        self.uart_write(b"AT+CCID\r\n")
-        self.read_line(timeout=2000)  # 6.2.23, 2sec max. response time
+        self._uart_write(b"AT+CCID\r\n")
+        self._read_line(timeout=2000)  # 6.2.23, 2sec max. response time
         iccid = self._buf.decode()
-        self.read_line()  # eat 'OK'
+        self._read_line()  # eat 'OK'
         return iccid
 
     @property
@@ -243,7 +244,7 @@ class FONA:
             # enable multi connection mode (3,1)
             if not self._send_check_reply(b"AT+CIPMUX=1", reply=REPLY_OK):
                 return False
-            self.read_line()
+            self._read_line()
 
             # enable receive data manually (7,2)
             if not self._send_check_reply(b"AT+CIPRXGET=1", reply=REPLY_OK):
@@ -273,14 +274,14 @@ class FONA:
             # send AT+CSTT,"apn","user","pass"
             self._uart.reset_input_buffer()
 
-            self.uart_write(b'AT+CSTT="' + apn_name)
+            self._uart_write(b'AT+CSTT="' + apn_name)
 
             if apn_user is not None:
-                self.uart_write(b'","' + apn_user)
+                self._uart_write(b'","' + apn_user)
 
             if apn_pass is not None:
-                self.uart_write(b'","' + apn_pass)
-            self.uart_write(b'"\r\n')
+                self._uart_write(b'","' + apn_pass)
+            self._uart_write(b'"\r\n')
 
             if not self._get_reply(REPLY_OK):
                 return False
@@ -351,7 +352,7 @@ class FONA:
             rssi = map_range(reply_num, 2, 30, -110, -54)
 
         # read out the 'ok'
-        self.read_line()
+        self._read_line()
         return rssi
 
     @property
@@ -370,7 +371,7 @@ class FONA:
             status = int(self._buf[10:11].decode("utf-8"))
             if status == 1:
                 status = 3  # assume 3D fix
-            self.read_line()
+            self._read_line()
         elif self._fona_type == FONA_3G_A or self._fona_type == FONA_3G_E:
             raise NotImplementedError(
                 "FONA 3G not currently supported by this library."
@@ -399,14 +400,14 @@ class FONA:
         if self._fona_type == FONA_808_V2:
             if not self._send_parse_reply(b"AT+CGPSPWR?", b"+CGPSPWR: ", ":"):
                 return False
-        self.read_line()
+        self._read_line()
         if not self._send_parse_reply(b"AT+CGNSPWR?", b"+CGNSPWR: ", ":"):
             return False
 
         state = self._buf
 
         if gps_on and not state:
-            self.read_line()
+            self._read_line()
             if self._fona_type == FONA_808_V2:
                 # try GNS
                 if not self._send_check_reply(b"AT+CGNSPWR=1", reply=REPLY_OK):
@@ -441,9 +442,9 @@ class FONA:
             return False
 
         # attempt to parse a response
-        self.read_line()
+        self._read_line()
         while not self._parse_reply(b"+CDNSGIP:", idx=2):
-            self.read_line()
+            self._read_line()
 
         return self._buf
 
@@ -474,35 +475,27 @@ class FONA:
         else:
             if not self._send_check_reply(b"AT+CNMI=2,0\r\n", reply=REPLY_OK):
                 return False
-
         return True
 
     def receive_sms(self):
         """Checks for a message notification from the FONA module,
         replies back with the a tuple containing (sender, message).
+
         """
+        if self._ri is not None: # poll the RI pin
+            if not self._ri.value:
+                return False, False
+        else: # poll the UART instead
+            if not self._uart.in_waiting:
+                return False, False
 
-        if self._ri.value == False: # RI is active High
-            # TODO: Verify timer is around 120mS
-            timer = 120
-            pulses = []
-            while timer > 0:
-                print(self._ri.value)
-                timer -= 1
-                time.sleep(0.001)
-
-        # TODO: Make optional, rely on polling RI instead.
-        if not self.in_waiting:
-            return False, False
-
-        self.read_line()
+        self._read_line() # parse the rcv'd URC
         if not self._parse_reply(b"+CMTI: ", idx=1):
             return False, False
         slot = self._buf
         sender, message = self.read_sms(slot)
 
-        # delete the message from the module to save memory
-        if not self.delete_sms(slot):
+        if not self.delete_sms(slot): # delete sms from module memory
             return False, False
 
         return sender, message.strip()
@@ -520,24 +513,24 @@ class FONA:
         if not self._send_check_reply(b"AT+CMGF=1", reply=REPLY_OK):
             return False
 
-        self.uart_write(b'AT+CMGS="+' + str(phone_number).encode() + b'"' + b"\r")
-        self.read_line()
+        self._uart_write(b'AT+CMGS="+' + str(phone_number).encode() + b'"' + b"\r")
+        self._read_line()
 
         # expect >
         if self._buf[0] != 62:
             # promoting mark ('>') not found
             return False
-        self.read_line()
+        self._read_line()
 
         # write out message and ^z
-        self.uart_write((message + chr(26)).encode())
+        self._uart_write((message + chr(26)).encode())
 
         if self._fona_type == FONA_3G_A or self._fona_type == FONA_3G_E:
             # eat 2x CRLF
-            self.read_line(200)
-            self.read_line(200)
+            self._read_line(200)
+            self._read_line(200)
         # read +CMGS, wait ~10sec.
-        self.read_line(10000)
+        self._read_line(10000)
 
         if not "+CMGS" in self._buf:
             return False
@@ -564,18 +557,18 @@ class FONA:
             ):
                 return self._buf
 
-        self.read_line()  # eat OK
+        self._read_line()  # eat OK
         if self._send_parse_reply(b"AT+CPMS?", b'"SM",', idx=1):
             return self._buf
 
-        self.read_line()  # eat OK
+        self._read_line()  # eat OK
         if self._send_parse_reply(b"AT+CPMS?", b'"SM_P",', idx=1):
             return self._buf
         return False
 
     def delete_all_sms(self):
         """Deletes all SMS messages on the FONA SIM."""
-        self.read_line()
+        self._read_line()
         if not self._send_check_reply(b"AT+CMGF=1", reply=REPLY_OK):
             return False
 
@@ -611,8 +604,8 @@ class FONA:
         if not self._send_check_reply(b"AT+CSDH=1", reply=REPLY_OK):
             return False
 
-        self.uart_write(b"AT+CMGR=" + str(sms_slot).encode() + b"\r\n")
-        self.read_line(1000)
+        self._uart_write(b"AT+CMGR=" + str(sms_slot).encode() + b"\r\n")
+        self._read_line(1000)
         resp = self._buf
 
         # get sender
@@ -630,7 +623,7 @@ class FONA:
         self._uart.readinto(self._buf)
         message = bytes(self._buf).decode()
         self._uart.reset_input_buffer()
-        self.read_line()  # eat 'OK'
+        self._read_line()  # eat 'OK'
 
         return sender, message
 
@@ -643,20 +636,20 @@ class FONA:
         if self._debug:
             print("*** Get socket")
 
-        self.uart_write(b"AT+CIPSTATUS\r\n")
-        self.read_line(100)  # OK
-        self.read_line(100)  # table header
+        self._uart_write(b"AT+CIPSTATUS\r\n")
+        self._read_line(100)  # OK
+        self._read_line(100)  # table header
 
         allocated_socket = 0
         for sock in range(0, FONA_MAX_SOCKETS):  # check if INITIAL state
-            self.read_line(100)
+            self._read_line(100)
             self._parse_reply(b"C:", idx=5)
             if self._buf.strip('"') == "INITIAL" or self._buf.strip('"') == "CLOSED":
                 allocated_socket = sock
                 break
         # read out the rest of the responses
         for _ in range(allocated_socket, FONA_MAX_SOCKETS):
-            self.read_line(100)
+            self._read_line(100)
         if self._debug:
             print("Allocated socket #%d" % allocated_socket)
         return allocated_socket
@@ -670,8 +663,8 @@ class FONA:
             sock_num < FONA_MAX_SOCKETS
         ), "Provided socket exceeds the maximum number of \
                                              sockets for the FONA module."
-        self.uart_write(b"AT+CIPSTATUS=" + str(sock_num).encode() + b"\r\n")
-        self.read_line(100)
+        self._uart_write(b"AT+CIPSTATUS=" + str(sock_num).encode() + b"\r\n")
+        self._read_line(100)
 
         self._parse_reply(b"+CIPSTATUS:", idx=3)
         return self._buf
@@ -687,10 +680,10 @@ class FONA:
                                              sockets for the FONA module."
         if not self._send_check_reply(b"AT+CIPSTATUS", reply=REPLY_OK, timeout=100):
             return False
-        self.read_line()
+        self._read_line()
 
         for state in range(0, sock_num + 1):  # read "C: <n>" for each active connection
-            self.read_line()
+            self._read_line()
             if state == sock_num:
                 break
         self._parse_reply(b"C:", idx=5)
@@ -699,7 +692,7 @@ class FONA:
 
         # eat the rest of the sockets
         for _ in range(sock_num, FONA_MAX_SOCKETS):
-            self.read_line()
+            self._read_line()
 
         if not "CONNECTED" in state:
             return False
@@ -724,8 +717,8 @@ class FONA:
         if self._debug:
             print("\t {} bytes available.".format(self._buf))
 
-        self.read_line()
-        self.read_line()
+        self._read_line()
+        self._read_line()
 
         return data
 
@@ -759,16 +752,16 @@ class FONA:
             )
 
         # Query local IP Address
-        self.uart_write(b"AT+CIFSR\r\n")
-        self.read_line()
+        self._uart_write(b"AT+CIFSR\r\n")
+        self._read_line()
 
         # Start connection
-        self.uart_write(b"AT+CIPSTART=" + str(sock_num).encode())
+        self._uart_write(b"AT+CIPSTART=" + str(sock_num).encode())
         if conn_mode == 0:
-            self.uart_write(b',"TCP","')
+            self._uart_write(b',"TCP","')
         else:
-            self.uart_write(b',"UDP","')
-        self.uart_write(dest.encode() + b'","' + str(port).encode() + b'"\r\n')
+            self._uart_write(b',"UDP","')
+        self._uart_write(dest.encode() + b'","' + str(port).encode() + b'"\r\n')
 
         if not self._expect_reply(REPLY_OK):
             return False
@@ -791,9 +784,9 @@ class FONA:
         ), "Provided socket exceeds the maximum number of \
                                              sockets for the FONA module."
 
-        self.uart_write(b"AT+CIPCLOSE=" + str(sock_num).encode() + b",")
-        self.uart_write(str(quick_close).encode() + b"\r\n")
-        self.read_line()
+        self._uart_write(b"AT+CIPCLOSE=" + str(sock_num).encode() + b",")
+        self._uart_write(str(quick_close).encode() + b"\r\n")
+        self._read_line()
         if not self._parse_reply(b"CLOSE OK", idx=0):
             return False
         return True
@@ -812,12 +805,12 @@ class FONA:
         if self._debug:
             print("* socket read")
 
-        self.uart_write(b"AT+CIPRXGET=2,")
-        self.uart_write(str(sock_num).encode())
-        self.uart_write(b",")
-        self.uart_write(str(length).encode() + b"\r\n")
+        self._uart_write(b"AT+CIPRXGET=2,")
+        self._uart_write(str(sock_num).encode())
+        self._uart_write(b",")
+        self._uart_write(str(length).encode() + b"\r\n")
 
-        self.read_line()
+        self._read_line()
 
         if not self._parse_reply(b"+CIPRXGET:"):
             return False
@@ -832,23 +825,23 @@ class FONA:
         :param bytes buffer: Bytes to write to socket.
 
         """
-        self.read_line()
+        self._read_line()
         assert (
             sock_num < FONA_MAX_SOCKETS
         ), "Provided socket exceeds the maximum number of \
                                              sockets for the FONA module."
 
         self._uart.reset_input_buffer()
-        self.uart_write(b"AT+CIPSEND=" + str(sock_num).encode())
-        self.uart_write(b"," + str(len(buffer)).encode() + b"\r\n")
-        self.read_line()
+        self._uart_write(b"AT+CIPSEND=" + str(sock_num).encode())
+        self._uart_write(b"," + str(len(buffer)).encode() + b"\r\n")
+        self._read_line()
 
         if self._buf[0] != 62:
             # promoting mark ('>') not found
             return False
 
-        self.uart_write(buffer + b"\r\n")
-        self.read_line(3000)
+        self._uart_write(buffer + b"\r\n")
+        self._read_line(3000)
 
         if "SEND OK" not in self._buf.decode():
             return False
@@ -857,12 +850,7 @@ class FONA:
 
     ### UART Reply/Response Helpers ###
 
-    @property
-    def in_waiting(self):
-        """Returns number of bytes available to be read in the input buffer."""
-        return self._uart.in_waiting
-
-    def uart_write(self, buffer):
+    def _uart_write(self, buffer):
         """UART ``write`` with optional debug that prints
         the buffer before sending.
         :param bytes buffer: Buffer of bytes to send to the bus.
@@ -879,7 +867,7 @@ class FONA:
         :param str divider: Separator
 
         """
-        self.read_line()
+        self._read_line()
         self._get_reply(send_data)
 
         if not self._parse_reply(reply_data, divider, idx):
@@ -897,11 +885,11 @@ class FONA:
         self._uart.reset_input_buffer()
 
         if data is not None:
-            self.uart_write(data + b"\r\n")
+            self._uart_write(data + b"\r\n")
         else:
-            self.uart_write(prefix + suffix + b"\r\n")
+            self._uart_write(prefix + suffix + b"\r\n")
 
-        return self.read_line(timeout)
+        return self._read_line(timeout)
 
     def _parse_reply(self, reply, divider=",", idx=0):
         """Attempts to find reply in UART buffer, reads up to divider.
@@ -927,7 +915,7 @@ class FONA:
 
         return True
 
-    def read_line(self, timeout=FONA_DEFAULT_TIMEOUT_MS, multiline=False):
+    def _read_line(self, timeout=FONA_DEFAULT_TIMEOUT_MS, multiline=False):
         """Reads one or multiple lines into the buffer. Optionally prints the buffer
         after reading.
         :param int timeout: Time to wait for UART serial to reply, in seconds.
@@ -976,7 +964,7 @@ class FONA:
         :param bytes reply: Expected response from module.
 
         """
-        self.read_line()
+        self._read_line()
         if send is None:
             if not self._get_reply(prefix=prefix, suffix=suffix, timeout=timeout):
                 return False
@@ -1017,16 +1005,16 @@ class FONA:
         """
         self._uart.reset_input_buffer()
 
-        self.uart_write(prefix + b'"' + suffix + b'"\r\n')
+        self._uart_write(prefix + b'"' + suffix + b'"\r\n')
 
-        return self.read_line(timeout)
+        return self._read_line(timeout)
 
     def _expect_reply(self, reply, timeout=10000):
         """Reads line from FONA module and compares to reply from FONA module.
         :param bytes reply: Expected reply from module.
 
         """
-        self.read_line(timeout)
+        self._read_line(timeout)
         if reply not in self._buf:
             return False
         return True
